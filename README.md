569388772110fe8787d9a125958bd95b1ad45e66
zhongzf <zhongzf@126.com>
2017-01-09 23:08:38 +0800

    use command line

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # ASP.NET Core demo
 
+Demo website is availabel here [here][1].
+
 ##
 <a href="https://heroku.com/deploy">
   <img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy">
 </a>
+
+---
--- src/aspnetcore-demo/Program.cs
+++ src/aspnetcore-demo/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace aspnetcore_demo
 {
@@ -11,7 +12,12 @@ namespace aspnetcore_demo
     {
         public static void Main(string[] args)
         {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
             var host = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
